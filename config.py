7916a6e7623ae2c1ddfd0d21afa8b887017ba835
Thi Ha Phuong Luu <PhuongLuu@my.unt.edu>
2025-10-27 02:30:24 -0500

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Settings:
-    def __init__(self):
-        # Đường dẫn tới database SQLite
-        self.db_path = os.getenv("DB_PATH", "totally_not_my_privateKeys.db")
-
-settings = Settings()
